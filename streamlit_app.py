5a65b6d50245956eee089dc9b0fe6a61bb6acb0b
georgemansibang <80440517+georgemansibang@users.noreply.github.com>
2024-06-18 11:02:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        ingredients_string += search_on + ' '
+        ingredients_string1 += fruit_chosen + ' '
+        ingredients_string2 += search_on + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -44,14 +44,18 @@ if ingredients_list:
         if search_on:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string2 + """','"""+name_on_order+"""')"""
         else:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string1 + """','"""+name_on_order+"""')"""
  
     #st.write(ingredient_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            #values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     #st.write(my_insert_stmt)
     #st.stop()
     #$st.write(my_insert_stmt)
